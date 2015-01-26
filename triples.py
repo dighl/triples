b2434c8903b8c5a095da87004607ae88c7ffa5b1
LinguList <mattis.list@uni-marburg.de>
2015-01-26 14:30:47 +0100

    fixes #6, python now basic language for providing the data remotely

--- triples.py
+++ triples.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python2
+#!/usr/bin/python2.6
 import cgi
 import sqlite3
+import datetime
 
 print "Content-type: text/plain; charset=utf-8"
-print
 
 # get the args of the url, convert nasty field storage to plain dictionary,
 # there is probably a better solution, but this works for the moment
@@ -25,21 +25,61 @@ db = sqlite3.connect(dbpath)
 cursor = db.cursor()
 
 # load the table if this is specified
-if 'file' in args:
+if 'tables' in args:
+    print 
+    for line in cursor.execute(
+    'select name from sqlite_master where name != "backup";'):
+        print line[0]
+# return most recent edits in the data
+elif 'date' in args:
+    print
+    cursor.execute(
+            'select ID,COL from backup where FILE="'+args['file']+'"'+\
+                    ' and datetime(DATE) > datetime('+args['date']+')'+\
+                    ' group by ID,COL limit 100;')
+    lines = cursor.fetchall()
+    data = dict([((a,b),c) for a,b,c in cursor.execute(
+            'select * from '+args['file']+';'
+            )])
+    for line in lines:
+        try:
+            val = data[line[0],line[1]].encode('utf-8')
+            print '{0}\t{1}\t{2}'.format(line[0], line[1], val) 
+        except KeyError:
+            pass
+        
+elif 'new_id' in args:
+    print
+    if args['new_id'] in ['new_id','newid','true']:
+        cursor.execute('select DISTINCT ID from '+args['file']+';')
+        linesA = [x[0] for x in cursor.fetchall()]
+        cursor.execute(
+            'select DISTINCT ID from backup where FILE = "'+args['file']+'";'
+            )
+        linesB = [x[0] for x in cursor.fetchall()]
+        maxA = max(linesA)
+        maxB = max(linesB)
+        if maxA >= maxB:
+            print str(maxA + 1)
+        else:
+            print str(maxB + 1)
+    else:
+        lines = [int(x[0]) for x in cursor.execute('select DISTINCT VAL from '+args['file']+\
+                ' where COL="'+args['new_id']+'";')]
+        
+        print str(max(lines)+1)
 
+elif 'file' in args:
+    print 'Content-Disposition: attachment; filename="triples.tsv"'
+    print
+    
     # get unique columns
     if not 'columns' in args:
         cols = [line[0] for line in cursor.execute(
             'select distinct COL from '+args['file']+';')]
     else:
         cols = args['columns'].split('|')
-
-    # get unique concept strings
-    #concepts = [line[0] for line in cursor.execute(
-    #    'select distinct CONCEPT from '+args['file']+';')]
-    #doculects = [line[0] for line in cursor.execute(
-    #    'select distinct DOCULECT from '+args['file']+';')]
-
+    
     print 'ID\t'+'\t'.join(cols)
     
     # if neither concepts or doculects are passed from the args, all ids are
@@ -76,15 +116,65 @@ if 'file' in args:
                 D[a] = {b:c.encode('utf-8')}
     # make object
     for idx in idxs:
-        print str(idx),
+        txt = str(idx)
         for col in cols:
             try:
-                print '\t'+D[idx][col],
+                txt += '\t'+D[idx][col]
             except:
-                print '\t',
-        print '\n',
-        
-if 'remote' in args:
+                txt += '\t'
+        print txt
 
-    with open(args['remote']) as f:
-        print(f.read())
+# XXX note that the following formula will bring much faster results:
+# select * from table where ID||":"||COL in ("X:Y");
+# with this formula we can limit the search space drastically, I think
+# addon: it seems this is much faster, but also the fact that:
+
+# * we print only once, XXX addon: this doesn't seem to be the case
+# * we use python for sampling, and
+# * we don't make many exact sqlite3 statements, this should be kept in mind,
+# since it may speed up all the processes!
+elif 'history' in args:
+    print
+    
+    if 'limit' in args:
+        limit = ' limit ' + args['limit']
+    else:
+        limit = ''
+    cursor.execute(
+            'select * from backup order by DATE DESC'+limit+';')
+    backup = cursor.fetchall()
+
+    # get ID restriction
+    idres = '("'+'","'.join([str(line[1])+':'+line[2] for line in backup])+'")'
+
+    # get the rest
+    tables = [] 
+    for line in cursor.execute(
+        'select name from sqlite_master where name != "backup";'):
+        tables += [line[0]]
+    
+    data = {}
+    for table in tables:
+        for a,b,c in cursor.execute(
+                'select * from '+table+' where ID||":"||COL in '+idres+';'):
+            try:
+                data[table][a,b] = c
+            except KeyError:
+                data[table] = {(a,b):c}
+    
+    txt = ''
+    for line in backup:
+        try:
+            new_val = data[line[0]][line[1],line[2]]
+        except KeyError:
+            new_val = '???'
+
+        print '{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}'.format(
+                line[0],
+                line[1],
+                line[2],
+                line[3].encode('utf-8'),
+                new_val.encode('utf-8'),
+                str(line[4]),
+                line[5])
+
---
+++ update.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python2.6
+import cgi
+import os
+import sqlite3
+import json
+from rechte import rights
+import datetime
+
+print "Content-type: text/plain; charset=utf-8"
+print
+
+# get the args of the url, convert nasty field storage to plain dictionary,
+# there is probably a better solution, but this works for the moment
+tmp_args = cgi.FieldStorage()
+args = {}
+for arg in tmp_args:
+    args[arg] = tmp_args[arg].value
+
+# check whether value was submitted to arguments, otherwise insert empty one
+if 'VAL' not in args: args['VAL'] = ''
+
+# check for dbase arg and switch the database in case an argument is provided
+if 'remote_dbase' in args:
+    dbpath = args['remote_dbase']+'.sqlite3' if not \
+            args['remote_dbase'].endswith('sqlite3') else \
+            args['remote_dbase']
+else:
+    dbpath = 'triples.sqlite3'
+
+# connect to db
+db = sqlite3.connect(dbpath)
+cursor = db.cursor()
+
+# get datetime
+now = str(datetime.datetime.now()).split('.')[0]
+
+# check for remote user
+if 'REMOTE_USER' in os.environ:
+    user = os.environ['REMOTE_USER']
+else:
+    user = 'unknown'
+
+# check whether user has the permissions
+if args['file'] in rights[user]:
+    pass
+else:
+    print 'ERROR: User does not have the permission to modify the data!'
+    raise 
+
+# go for update
+if 'update' in args:
+
+    # allow for multiple posting of values separated by a "'"
+    idxs = args['ID'].split('|')
+    cols = args['COL'].split('|')
+    vals = args['VAL'].split('|') # be careful with this splitter!
+
+    # iterate over the entries
+    if len(idxs) == len(cols) == len(vals):
+        pass
+    else:
+        print 'ERROR: wrong values submitted'
+        raise
+
+    # start iteration
+    for idx,col,val in zip(idxs,cols,vals):
+        # get original data value
+        try:
+            orig_val = [x for x in cursor.execute(
+                'select VAL from ' + args['file'] + ' where ID=' +\
+                        idx + ' and COL like "'+col+'";')][0][0].encode('utf-8')
+            
+            qstring = 'update '+args['file'] + ' set VAL="'+val+'" where ID='+idx+' and COL="'+col+'";'
+            cursor.execute(
+                    qstring
+                    )
+            print 'UPDATE: Modification successful replace "{0}" with "{1}" on {2}.'.format(
+                    orig_val,
+                    val,
+                    now)
+                    
+        except IndexError:
+            orig_val = '!newvalue!'
+            
+            # create new datum if value has not been retrieved
+            cursor.execute(
+                    'insert into '+args['file'] + ' values(' +\
+                            idx + ',"' + col + '","' +\
+                            val + '");')
+            print 'INSERTION: Successfully inserted {0} on {1}'.format(
+                    val, now)
+
+        # insert the backup line
+        cursor.execute(
+                'insert into backup values(?,?,?,?,strftime("%s","now"),?);',
+                (
+                    args['file'],
+                    idx,
+                    col,
+                    orig_val,
+                    user
+                    ))
+
+    db.commit()
+
+elif 'delete' in args:
+    lines = [line for line in cursor.execute(
+        'select * from '+args['file'] +' where ID='+args['ID']+';'
+        )]
+    for idx,col,val in lines:
+        cursor.execute(
+                'insert into backup values(?,?,?,?,strftime("%s","now"),?);',
+                (args['file'],idx, col, val, user))
+        cursor.execute(
+                'delete from '+args['file'] + ' where ID='+args['ID']+';')
+    print 'DELETION: Successfully deleted all entries for ID {0} on {1}.'.format(
+            args['ID'],
+            now)
+
+    db.commit()
+
+db.close()
